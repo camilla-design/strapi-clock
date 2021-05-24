0f5b7b6be466244d2f9f0f9a6a772849ee975110
Atlungstad Brenneri <camilla_repp@hotmail.com>
2021-05-24 19:29:31 +0200

    upload provider

--- config/plugins.js
+++ config/plugins.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
     upload: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: env(AWS_ACCESS_KEY_ID),
+        accessKeyId: env(AWS_ACCESS_KEY),
         secretAccessKey: env(AWS_ACCESS_SECRET),
         region: AWS_REGION,
         params: {
