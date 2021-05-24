704f7c1b8e4a488f1ac98e9b16d902211004ee4c
Atlungstad Brenneri <camilla_repp@hotmail.com>
2021-05-24 19:18:57 +0200

    added upload provider

--- config/plugins.js
+++ config/plugins.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
     upload: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: env('AKIAWPCLKXNK2BAMIEDY'),
-        secretAccessKey: env('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'),
-        region: 'eu-north-1',
+        accessKeyId: env(AWS_ACCESS_KEY_ID),
+        secretAccessKey: env(AWS_ACCESS_SECRET),
+        region: AWS_REGION,
         params: {
-          Bucket: 'heals-shop-uploder',
+          Bucket: AWS_BUCKET,
         },
       },
     },
