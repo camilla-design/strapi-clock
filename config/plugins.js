0ba46f2d4cd935af208d634bc77b12eaeeccbdc1
Atlungstad Brenneri <camilla_repp@hotmail.com>
2021-05-24 18:59:15 +0200

    added upload provider

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+    upload: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AKIAWPCLKXNK2BAMIEDY'),
+        secretAccessKey: env('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'),
+        region: 'eu-north-1',
+        params: {
+          Bucket: 'heals-shop-uploder',
+        },
+      },
+    },
+  });
