8dc7cc5fe2d0b0902c17bdc9a5675e9b4ec499b6
Atlungstad Brenneri <camilla_repp@hotmail.com>
2021-05-25 12:20:55 +0200

    added strapi-utils

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -1,8 +1,22 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.product.create(data, { files });
+    } else {
+      entity = await strapi.services.product.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.product });
+  },
+};
